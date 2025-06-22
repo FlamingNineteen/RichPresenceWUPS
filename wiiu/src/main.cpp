110f13c391831d2f5422d73c810a1b9605ac2c27
FlamingNineteen <flamingnineteen@gmail.com>
2025-06-22 11:34:27 -0700

    update via tcp 5000 instead of ftp

--- wiiu/src/main.cpp
+++ wiiu/src/main.cpp
@@ -1,22 +1,37 @@
+#include <atomic>
+#include <chrono>
 #include <malloc.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdio.h>
 #include <time.h>
+#include <thread>
+#include <unistd.h>
 
 #include <wups.h>
 #include <wups/config.h>
 
+#include <wut.h>
+
 #include <coreinit/cache.h>
 #include <coreinit/title.h>
 #include <coreinit/time.h>
+#include <coreinit/thread.h>
+#include <coreinit/memdefaultheap.h>
 
-#include <wut.h>
+#include <sys/socket.h>
 
-#include <padscore/wpad.h>
-#include <padscore/kpad.h>
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+// #include <padscore/wpad.h>
+// #include <padscore/kpad.h>
 
 #include <nn/acp/client.h>
 #include <nn/acp/title.h>
+
+#define STACK_SIZE 0x2000
 
 /**
     Mandatory plugin information.
@@ -24,36 +39,23 @@
 **/
 WUPS_PLUGIN_NAME("RichPresence");
 WUPS_PLUGIN_DESCRIPTION("Discord Rich Presence for the Wii U.");
-WUPS_PLUGIN_VERSION("v1.0");
+WUPS_PLUGIN_VERSION("v1.5");
 WUPS_PLUGIN_AUTHOR("Flaming19");
 WUPS_PLUGIN_LICENSE("GNU");
 
-WUPS_USE_WUT_DEVOPTAB();             // Use the wut devoptabs
+WUPS_USE_WUT_DEVOPTAB();           // Use the wut devoptabs
 WUPS_USE_STORAGE("rich_presence"); // Unique id for the storage api
 
-// Global scope
-const char* app;
-std::string ctrls;
-bool running =         false;
-WPADChan channels[7] = {WPAD_CHAN_0, WPAD_CHAN_1, WPAD_CHAN_2, WPAD_CHAN_3, WPAD_CHAN_4, WPAD_CHAN_5, WPAD_CHAN_6};
+// Settings
+struct WURP_CONFIG {
+    int time;
+    bool enabled;
+    bool notify;
+};
 
-void WriteToSDCard(std::string data, std::string num) {
-    const char* filePath = "/vol/external01/rich_presence.txt";
-    data += "^" + std::to_string(time(NULL)) + "^" + num + "\n";
-    const char* datacs = data.c_str();
-
-    FILE* file = fopen(filePath, "w");
-    if (!file) {
-        return;
-    }
-
-    // Overwrite file and make it blank
-    fprintf(file, "%s", "");
-    size_t dataSize = strlen(datacs);
-
-    fwrite(datacs, 1, dataSize, file);
-    fclose(file);
-}
+// Global variables
+std::string app = "";
+std::jthread tthread;
 
 std::string GetNameOfCurrentApplication() {
     std::string result;
@@ -68,43 +70,62 @@ std::string GetNameOfCurrentApplication() {
     return result;
 }
 
-int ctrlNum() {
-    WPADExtensionType extType;
-    int c = 0;
-    for (int i = 0; i < 7; i++) {
-        int32_t result = WPADProbe(channels[i], &extType);
-        if (result != -1) {c++;}
-    }
-    return c;
+// Broadcast over TCP 5000
+void Broadcast(const std::string& json) {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) return;
+
+    int broadcast = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+
+    sockaddr_in dest {};
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(5005);
+    dest.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    sendto(sock, json.c_str(), json.size(), 0, reinterpret_cast<sockaddr*>(&dest), sizeof(dest));
+    close(sock);
 }
 
-void OnControllerConnect(WPADChan chan, int32_t status) {
-    ctrls = std::to_string(ctrlNum());
-    WriteToSDCard(app, ctrls);
+void GameLoop(std::stop_token stoken) {
+    while (!stoken.stop_requested()) {
+        Broadcast("LOOP"); // Debug
+        if (app != "") {
+            std::string json = "\"app\":" + app + ",\"time\":" + std::to_string(time(NULL));
+            Broadcast(json);
+        }
+        // Five second interval
+        for (int i=0; i<1000 && !stoken.stop_requested(); i++) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+    }
+    Broadcast("QUIT"); // Debug
 }
 
 INITIALIZE_PLUGIN() {
-    WPADInit();
-    // Register the controller connection callback for all channels
-    for (int i = 0; i < 7; i++) {
-        WPADSetConnectCallback(channels[i], OnControllerConnect);
-    }
-
-    // Reset rich_presence.txt
-    FILE* file = fopen("/vol/external01/rich_presence.txt", "w");
-    fwrite("", 1, 0, file);
-    fclose(file);
-}
-
-DEINITIALIZE_PLUGIN() {
-    WPADShutdown();
+    Broadcast("INIT PLUGIN"); // Debug
+    WURP_CONFIG settings;
+    WUPSStorageAPI::Get("time", settings.time);
+    WUPSStorageAPI::Get("enabled", settings.enabled);
 }
 
 ON_APPLICATION_START() {
-    ctrls = std::to_string(ctrlNum());
-    app = GetNameOfCurrentApplication().c_str();
+    Broadcast("APP START"); // Debug
+    app = GetNameOfCurrentApplication();
     if (GetNameOfCurrentApplication() == "Health and Safety Information") {
         app = "Homebrew Application";
     }
-    WriteToSDCard(app, ctrls);
+    Broadcast("APP SET " + app); // Debug
+    if (tthread.joinable()) tthread.request_stop();
+    tthread = std::jthread(GameLoop);
+}
+
+ON_APPLICATION_REQUESTS_EXIT() {
+    Broadcast("APP REQ EXIT"); // Debug
+    if (tthread.joinable()) tthread.request_stop();
+}
+
+DEINITIALIZE_PLUGIN() {
+    Broadcast("DEINIT PLUGIN"); // Debug
+    if (tthread.joinable()) tthread.request_stop();
 }
