543bd026fbd0ab56690a61a1cc5e6c29ca9ccf32
FlamingNineteen <flamingnineteen@gmail.com>
2025-12-27 15:14:25 -0800

    Add Python script

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This plugin uses UDP port 5005 to communicate with an application on your computer to set Discord Rich Presence for the user. The activity is set based on the application currently being played, the time the application was loaded, and the amount of controllers connected.
 
+> [!NOTE]
+> Elapsed time may not show up correctly because of your Wii U's time. To offset the elapsed time that shows up in Discord by a certain amount of hours, open the plugin configuration menu and change the setting. If elapsed time displays `0:00:00`, you need to change the setting to a negative number. If elapsed time displays hours ahead of your actual play time, you need to change the setting to a positive number.
+
 ## Installation
 (`[ENVIRONMENT]` is a placeholder for the actual environment name.)
 
@@ -10,14 +13,20 @@ This plugin uses UDP port 5005 to communicate with an application on your comput
 3. Keep the executable on your computer.
 4. Requires the [WiiUPluginLoaderBackend](https://github.com/wiiu-env/WiiUPluginLoaderBackend) in `sd:/wiiu/environments/[ENVIRONMENT]/modules`.
 
-## Usage
+## Usage (Executable)
 Start your Wii U with the environment you placed the plugin in, and run the executable file with the Discord app open.
 
 > [!IMPORTANT]
 > On Linux, if you get the error `Segmentation fault (core dumped)`, use `sudo`.
 
-> [!NOTE]
-> Elapsed time may not show up correctly because of your Wii U's time. To offset the elapsed time that shows up in Discord by a certain amount of hours, open the plugin configuration menu and change the setting. If elapsed time displays `0:00:00`, you need to change the setting to a negative number. If elapsed time displays hours ahead of your actual play time, you need to change the setting to a positive number.
+## Usage (Python)
+Make sure that [`pypresence`](https://github.com/qwertyquerty/pypresence) and `requests` are installed
+
+```
+pip install requests pypresence
+```
+
+Start your Wii U with the environment you placed the plugin in, and run the executable file with the Discord app open.
 
 ## Contribute
 The plugin is missing images of many Wii U games. If you are interested in adding game images, and have a Github account, check out the [image repository](https://github.com/flamingnineteen/RichPresenceWUPS-DB) for this plugin.
---
+++ discord-script.py
@@ -0,0 +1,103 @@
+import asyncio
+import json
+import requests
+import socket
+
+from datetime import datetime
+from pypresence import Presence
+from pypresence.types import ActivityType, StatusDisplayType
+
+APP_ID = "1353248127469228074"
+REPO = "flamingnineteen/richpresencewups-db"
+
+# Connect to Discord
+client = Presence(client_id = APP_ID)
+client.connect()
+print("Connected to Discord")
+
+# Bind Socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = socket.gethostbyname(socket.gethostname())
+
+sock.bind((ip, 5005))
+
+# Recieve Image URLs
+titles = {}
+try:
+    req = requests.get(f"https://raw.githubusercontent.com/{REPO}/main/titles.json")
+    titles = json.loads(req.text)
+    print("Successfully fetched titles.json!")
+except:
+    print("Error fetching titles.json. Using default image.")
+
+def parse(msg):
+    p = msg.split('\'')[1]
+    try:
+        i = json.loads(p)
+    except:
+        i = {}
+    return i
+
+def toepoch(e):
+    dt = datetime.now().astimezone()
+    return e - int(dt.utcoffset().total_seconds())
+
+idle = True
+
+async def clearPresence():
+    global idle, client
+    allow = False
+    while 1:
+        await asyncio.sleep(5)
+        if idle:
+            if allow:
+                await asyncio.to_thread(client.clear)
+                print("Cleared Rich Presence")
+                while idle:
+                    pass
+            else:
+                allow = True
+        else:
+            allow = False
+        idle = True
+
+asyncio.run(clearPresence())
+
+while 1:
+    print("Waiting to recieve message")
+    msg = sock.recv(1024)
+    data = parse(str(msg))
+    print(f"Recieved: {data}")
+    idle = False
+
+    try:
+        if (data["sender"] == "Wii U"):
+            img = ""
+            try:
+                img = f"https://raw.githubusercontent.com/{REPO}/main/icons/{titles[data["long"]]}"
+            except:
+                img = "preview"
+
+            if data["nnid"] == '':
+                client.update(
+                    activity_type=       ActivityType.PLAYING,
+                    status_display_type= StatusDisplayType.STATE,
+                    state=               data["app"],
+                    start=               toepoch(data["time"]),
+                    large_image=         img,
+                    party_size=          [data["ctrls"] + 1 if data["ctrls"] > -2 else 0, 4 if data["ctrls"] < 4 else 8]
+                )
+            else:
+                client.update(
+                    activity_type=       ActivityType.PLAYING,
+                    status_display_type= StatusDisplayType.STATE,
+                    state=               data["app"],
+                    details=             f"Network ID: {data["nnid"]}",
+                    start=               toepoch(data["time"]),
+                    large_image=         img,
+                    party_size=          [data["ctrls"] + 1 if data["ctrls"] > -2 else 0, 4 if data["ctrls"] < 4 else 8]
+                )
+    except:
+        print("Updated Rich Presence")
+
+sock.close()
