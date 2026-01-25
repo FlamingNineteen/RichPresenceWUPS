4e1b494b3b07ea800971906f8bc2044f85dfcd94
FlamingNineteen <flamingnineteen@gmail.com>
2026-01-25 11:07:45 -0800

    Bind to all interfaces

--- discord-script.py
+++ discord-script.py
@@ -22,17 +22,16 @@ while disconnected:
         disconnected = False
     except:
         print("Failed to connect to Discord. Retrying...")
-        time.sleep(1)
+        time.sleep(2)
 print("Connected to Discord")
 
 # Bind Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = socket.gethostbyname(socket.gethostname())
 
 binded = False
 while not binded:
     try:
-        sock.bind((ip, 5005))
+        sock.bind(('', 5005))
         binded = True
         print("Binded to UDP port 5005")
     except:
